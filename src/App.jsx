80e6aead7bea6b1b444f801cfd0b35178c9c9ce7
manoj-101-dev <manoj19545@gmail.com>
2024-10-28 13:15:27 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import EntrancePage from "./components/EntrancePage";
 import ProductList from "./components/ProductList";
 import MyBookings from "./components/MyBookings";
 import CartPage from "./components/CartPage";
+import Success from "./components/Success";
+import Cancel from "./components/Cancel";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
@@ -18,6 +20,8 @@ const App = () => {
         <Route path="/products" element={<ProductList />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
       </Routes>
     </Router>
   );
